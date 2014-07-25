d9395d6428fa1b1b8c84bc08abb41cc801f4b57c
Sam Horn <shorn2432@gmail.com>
2014-07-25 18:05:16 -0400

    Test
    Age stuff

---
+++ test.py
@@ -0,0 +1,25 @@
+print("hello world") 
+
+name = input ("What is your name")
+print ("That's interesting" + name)
+
+age = input ("What is your age?").strip()
+age1 = input ("That's cool, what is your friends age").strip()
+
+age = int(age)
+age1 = int(age1)
+
+years_apart = abs (age1 - age)
+
+print ("You are {} years apart".format(years_apart))
+
+
+
+
+
+
+
+
+
+
+
